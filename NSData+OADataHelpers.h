0c49a7c718c5d2f859332aea78d90fbd823483b1
Mac_lyf <mac_lyf@Mac-lyfdeMacBook-Air-3.local>
2017-08-12 17:37:02 +0800

    用于过滤非法utf8字符数据

---
+++ NSData+OADataHelpers.h
@@ -0,0 +1,13 @@
+//
+//  NSData+OADataHelpers.h
+//  FamilyHealth
+//
+//  Created by Mac_lyf on 2017/7/20.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (OADataHelpers)
+- (NSData*) dataByHealingUTF8Stream;
+- (NSString*) UTF8String;
+@end
